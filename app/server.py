d817bc6c7bbf145d8cbbd8caa77f82e2a2200903
cphung3 <35232974+cphung3@users.noreply.github.com>
2020-05-21 20:12:36 -0700

    Make Google drive link downloadable

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1dJrl4WN-1mDCDQYdDQkQ1Y_qCMp-WKSm'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1dJrl4WN-1mDCDQYdDQkQ1Y_qCMp-WKSm'
 export_file_name = 'export.pkl'
 
 classes = ['cardboard', 'glass', 'metal','paper', 'plastic', 'trash']
